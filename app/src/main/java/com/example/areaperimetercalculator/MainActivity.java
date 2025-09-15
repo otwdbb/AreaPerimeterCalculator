7d61002e75974c94c6e26c6083df4051d37e8f88
Zmoore-MCCC <143642207+Zmoore-MCCC@users.noreply.github.com>
2025-09-15 15:18:34 -0400

    square rectangle done
    square rectangle done

--- app/src/main/java/com/example/areaperimetercalculator/MainActivity.java
+++ app/src/main/java/com/example/areaperimetercalculator/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.areaperimetercalculator;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    //rectangle and square
+    //area = length and width
+    //perimeter = length and width
+
+    //circle
+    //area = radius
+    //circumference = radius
+
+    //triangle
+    //area = base and height
+    //perimeter = side a, side b, and side c
+    EditText et_j_length;
+    EditText et_j_width;
+    ConstraintLayout cons_j_squareRectView;
+    Spinner sp_j_shapes;
+    TextView tv_j_areaPerimeter;
+    ArrayAdapter<String> spinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +47,130 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        et_j_length = findViewById(R.id.et_v_length);
+        et_j_width  = findViewById(R.id.et_v_width);
+        cons_j_squareRectView = findViewById(R.id.cont_v_squareRectangle);
+        sp_j_shapes = findViewById(R.id.sp_v_shapes);
+        tv_j_areaPerimeter = findViewById(R.id.tv_v_computedValues);
+
+        //Because we are making a simple drop down menu (spinner) that will only contain
+        //strings as options.  We can use a string array with the built-in array adapter
+        //to populate the spinner.
+
+        //we will use this to populate our spinner
+        String[] shapes = new String[]{"Square", "Rectangle", "Circle", "Triangle"};
+
+        //adapter is what links the java code with the xml for the spinner
+        spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,shapes);
+
+        sp_j_shapes.setAdapter(spinnerAdapter);
+
+        setupSpinnerChangeListener();
+        textChangeListenerSquareRect();
+    }
+
+    public void setupSpinnerChangeListener()
+    {
+        sp_j_shapes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //0-3 indexes
+                if(position == 0)
+                {
+                    //square
+                    cons_j_squareRectView.setVisibility(View.VISIBLE);
+                    //no showing for circle and triangle
+                }
+                else if(position == 1)
+                {
+                    //rectangle
+                    cons_j_squareRectView.setVisibility(View.VISIBLE);
+
+                    //no showing for circle and triangle
+                }
+                else if(position == 2)
+                {
+                    //Circle
+
+                    //no showing for rectangle and triangle
+
+                    hideConstraintView(cons_j_squareRectView);
+                }
+                else if (position == 3)
+                {
+                    //Triangle
+
+                    //no showing for circle and rectangle
+                    hideConstraintView(cons_j_squareRectView);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    public void hideConstraintView(ConstraintLayout cl)
+    {
+        cl.setVisibility(View.INVISIBLE);
+    }
+
+    public void textChangeListenerSquareRect()
+    {
+        et_j_width.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setAreaAndPerimeterSquareRect(et_j_length.getText().toString(),et_j_width.getText().toString());
+            }
+        });
+
+        et_j_length.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setAreaAndPerimeterSquareRect(et_j_length.getText().toString(),et_j_width.getText().toString());
+            }
+        });
+    }
+
+    public void setAreaAndPerimeterSquareRect(String lengthS, String widthS)
+    {
+
+        if(lengthS.isEmpty() || widthS.isEmpty())
+        {
+            return;
+        }
+        //convert the string to an integer so we can do math.
+        double length = Integer.parseInt(lengthS);
+        double width = Integer.parseInt(widthS);
+
+        double area = length * width;
+
+        double perimeter = length + length + width + width;
+
+        tv_j_areaPerimeter.setText("Area = " + area + "\nPerimeter = " + perimeter);
     }
 }
